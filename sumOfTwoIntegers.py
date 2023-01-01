a00e8b8caf906652d66b65a767bc5ba2550a15a3
colorfulcodes <codescolorful@gmail.com>
2023-01-01 17:38:36 -0500

    feat: solving without bitwise cuz nah

---
+++ sumOfTwoIntegers.py
@@ -0,0 +1,13 @@
+def getSum(a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        list=[a,b]
+        return sum(list) #hehe
+
+
+
+
+# With Bitshifting: https://leetcode.com/problems/sum-of-two-integers/solutions/776952/python-best-leetcode-371-explanation-for-python/
