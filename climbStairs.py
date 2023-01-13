2fad7643b991a9331c199b404d89f2cae850b87f
colorfulcodes <codescolorful@gmail.com>
2023-01-12 20:16:28 -0500

    feat: DP version

--- climbStairs.py
+++ climbStairs.py
@@ -13,3 +13,19 @@ def climbStairs(n):
 climbStairs(2)
 climbStairs(4)
 climbStairs(6)
+
+# DP
+def climbStairs(n):
+    if n==0: return 0
+    if n==1: return 1
+    if n==2: return 2
+    dp = [0]*(n+1) # considering zero steps we need n+1 places
+    dp[1]= 1
+    dp[2] = 2
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] +dp[i-2]
+    print(dp)
+    return dp[n]
+        
+climbStairs(6)
+climbStairs(4)
